1c1ef61aa4b3bb77c7f6fc7b42185f17cb8e011b
Tetsuro Matsumura <matsumura.t.lab@gmail.com>
2012-04-09 15:31:39 +0900

    add CMake file

---
+++ build/CMakeFiles/mylisp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mylisp.dir/src/main.c.o"
+  "mylisp.pdb"
+  "mylisp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/mylisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
