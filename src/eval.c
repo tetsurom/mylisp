29936174acdb5463b54e255d96bb74be7ae1537e
Tetsuro Matsumura <matsumura.t.lab@gmail.com>
2012-05-01 15:21:47 +0900

    v3 using label pointe

--- src/eval.c
+++ src/eval.c
@@ -37,121 +37,150 @@ void lisp_eval(lisp_t* L, cons_t* tree)
 #define OP_2STACKCMP(top, op) { *(top-1) = *(top-1) op *top; top -= 1; }
 #define PUSH(top, v) { *(++top) = v; }
 
+#define LABELP(x) (&&L_ ## x)
+#define CASE(x) L_ ## x
+#define JUMP(x) goto *jumptable[x]
+
+
 static void lisp_execute(lisp_t* L, lisp_mn_t* code_mn, int* sp_funcparam)
 {
+    static void* jumptable[] = {
+        LABELP(LC_PUSH),
+        LABELP(LC_LOADP),
+        LABELP(LC_LOADV),
+        LABELP(LC_LOADVS),
+        LABELP(LC_CALL),
+        LABELP(LC_CALLS),
+        LABELP(LC_SETQ),
+        LABELP(LC_IFNIL),
+        LABELP(LC_JUMP),
+        LABELP(LC_ADD),
+        LABELP(LC_SUB),
+        LABELP(LC_MUL),
+        LABELP(LC_DIV),
+        LABELP(LC_EQ),
+        LABELP(LC_LS),
+        LABELP(LC_GR),
+        LABELP(LC_LSEQ),
+        LABELP(LC_GREQ),
+        LABELP(LC_NEQ),
+        LABELP(LC_ADDC),
+        LABELP(LC_SUBC),
+        LABELP(LC_MULC),
+        LABELP(LC_DIVC),
+        LABELP(LC_EQC),
+        LABELP(LC_LSC),
+        LABELP(LC_GRC),
+        LABELP(LC_LSEQC),
+        LABELP(LC_GREQC),
+        LABELP(LC_NEQC),
+        LABELP(LC_RET),
+    };
     lisp_mn_t* code_head = code_mn;
     istack_t* stack = L->g_stack;
     int* data = stack->data;
     int* top = stack->data + stack->top;
-
-    assert(code_mn);
-    for(;; ++code_mn){
-        switch(code_mn->opcode){
-        case LC_PUSH:
-            PUSH(top, code_mn->ioperand);
-            break;
-        case LC_LOADP:
-            PUSH(top, sp_funcparam[code_mn->ioperand]);
-            break;
-        case LC_LOADV:
-            PUSH(top, *(int*)code_mn->poperand);
-            break;
-        case LC_LOADVS:
-            break;
-        case LC_CALL:
-        {
-            lisp_func_t* function = (lisp_func_t*)code_mn->poperand;
-            int argc = function->argc;
-            int* param_top = top - argc + 1;
-            stack->top = (int)(top - data);
-            //printf("%s(%i; %i, %i) ", function->name, function->argc, *param_top, *(param_top + 1));
-            lisp_execute(L, function->address, param_top);
-            //printf(" -> %i\n", data[stack->top]);
-            if(argc > 0){
-                *param_top = data[stack->top];
-                top = param_top;
-            }
-            break;
+    JUMP(code_mn->opcode);
+    CASE(LC_PUSH):
+        PUSH(top, code_mn->ioperand);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_LOADP):
+        PUSH(top, sp_funcparam[code_mn->ioperand]);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_LOADV):
+        PUSH(top, *(int*)code_mn->poperand);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_LOADVS):
+        JUMP((++code_mn)->opcode);
+    CASE(LC_CALL):
+    {
+        lisp_func_t* function = (lisp_func_t*)code_mn->poperand;
+        int argc = function->argc;
+        int* param_top = top - argc + 1;
+        stack->top = (int)(top - data);
+        //printf("%s(%i; %i, %i) ", function->name, function->argc, *param_top, *(param_top + 1));
+        lisp_execute(L, function->address, param_top);
+        //printf(" -> %i\n", data[stack->top]);
+        if(argc > 0){
+            *param_top = data[stack->top];
+            top = param_top;
         }
-        case LC_CALLS:
-            break;
-        case LC_SETQ:
-            break;
-        case LC_IFNIL:
-            if(!*(top--)){
-                code_mn = code_head + code_mn->ioperand - 1;
-            }
-            break;
-        case LC_JUMP:
-            code_mn = code_head + code_mn->ioperand - 1;
-            break;
-        case LC_ADD:
-            OP_2STACK(top, +);
-            break;
-        case LC_SUB:
-            OP_2STACK(top, -);
-            break;
-        case LC_MUL:
-            OP_2STACK(top, *);
-            break;
-        case LC_DIV:
-            OP_2STACK(top, /);
-            break;
-        case LC_EQ:
-            OP_2STACKCMP(top, ==);
-            break;
-        case LC_LS:
-            OP_2STACKCMP(top, <);
-            break;
-        case LC_GR:
-            OP_2STACKCMP(top, >);
-            break;
-        case LC_LSEQ:
-            OP_2STACKCMP(top, <=);
-            break;
-        case LC_GREQ:
-            OP_2STACKCMP(top, >=);
-            break;
-        case LC_NEQ:
-            OP_2STACKCMP(top, !=);
-            break;
-        case LC_ADDC:
-            *top += code_mn->ioperand;
-            break;
-        case LC_SUBC:
-            *top -= code_mn->ioperand;
-            break;
-        case LC_MULC:
-            *top *= code_mn->ioperand;
-            break;
-        case LC_DIVC:
-            *top /= code_mn->ioperand;
-            break;
-        case LC_EQC:
-            *top = *top == code_mn->ioperand;
-            break;
-        case LC_LSC:
-            *top = *top < code_mn->ioperand;
-            break;
-        case LC_GRC:
-            *top = *top > code_mn->ioperand;
-            break;
-        case LC_LSEQC:
-            *top = *top <= code_mn->ioperand;
-            break;
-        case LC_GREQC:
-            *top = *top <= code_mn->ioperand;
-            break;
-        case LC_NEQC:
-            *top = *top != code_mn->ioperand;
-            break;
-        case LC_RET:
-        case LC_END:
-            stack->top = (int)(top - data);
-            return;
-        default:
-            break;
-        }
+        JUMP((++code_mn)->opcode);
     }
+    CASE(LC_CALLS):
+        JUMP((++code_mn)->opcode);
+    CASE(LC_SETQ):
+        JUMP((++code_mn)->opcode);
+    CASE(LC_IFNIL):
+        if(!*(top--)){
+            code_mn = code_head + code_mn->ioperand - 1;
+        }
+        JUMP((++code_mn)->opcode);
+    CASE(LC_JUMP):
+        code_mn = code_head + code_mn->ioperand - 1;
+        JUMP((++code_mn)->opcode);
+    CASE(LC_ADD):
+        OP_2STACK(top, +);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_SUB):
+        OP_2STACK(top, -);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_MUL):
+        OP_2STACK(top, *);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_DIV):
+        OP_2STACK(top, /);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_EQ):
+        OP_2STACKCMP(top, ==);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_LS):
+        OP_2STACKCMP(top, <);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_GR):
+        OP_2STACKCMP(top, >);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_LSEQ):
+        OP_2STACKCMP(top, <=);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_GREQ):
+        OP_2STACKCMP(top, >=);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_NEQ):
+        OP_2STACKCMP(top, !=);
+        JUMP((++code_mn)->opcode);
+    CASE(LC_ADDC):
+        *top += code_mn->ioperand;
+        JUMP((++code_mn)->opcode);
+    CASE(LC_SUBC):
+        *top -= code_mn->ioperand;
+        JUMP((++code_mn)->opcode);
+    CASE(LC_MULC):
+        *top *= code_mn->ioperand;
+        JUMP((++code_mn)->opcode);
+    CASE(LC_DIVC):
+        *top /= code_mn->ioperand;
+        JUMP((++code_mn)->opcode);
+    CASE(LC_EQC):
+        *top = *top == code_mn->ioperand;
+        JUMP((++code_mn)->opcode);
+    CASE(LC_LSC):
+        *top = *top < code_mn->ioperand;
+        JUMP((++code_mn)->opcode);
+    CASE(LC_GRC):
+        *top = *top > code_mn->ioperand;
+        JUMP((++code_mn)->opcode);
+    CASE(LC_LSEQC):
+        *top = *top <= code_mn->ioperand;
+        JUMP((++code_mn)->opcode);
+    CASE(LC_GREQC):
+        *top = *top <= code_mn->ioperand;
+        JUMP((++code_mn)->opcode);
+    CASE(LC_NEQC):
+        *top = *top != code_mn->ioperand;
+        JUMP((++code_mn)->opcode);
+    CASE(LC_RET):
+        stack->top = (int)(top - data);
+        return;
 }
 
